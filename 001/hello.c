dbeaa1d80b8ab751ee11bf50d5ff503cd9ae21f2
oleg <oleg@oSvoj.ru>
2022-09-07 23:22:04 +0300

    Добавлено, gettext и doxygen сделана библиотека libhello

--- 001/hello.c
+++ 001/hello.c
@@ -3,12 +3,31 @@ hello.c
 author: oleg <oleg@osvoj.ru>
 license: MIT
 */
+/*
+Подключаем gettext
+*/
+#include <libintl.h>
 
+/*!
+\brief Макрос _ к функции gettext
+
+\param[out] String Возвращает текст в текущей локали
+\param[in] String Принимает строку, кодового текста
+*/
+#define _(String) gettext(String)
 
 #include <stdio.h> /*  подключение библиотеки ввода вывода  */
 
+
+
+/*!
+\brief Выводит надпись "Привет мир!!!"
+
+\param[out] int Возвращает код ошибки
+\param[in]  void Не имеет входящих параметров
+*/
 int hello (void) /* объявление основной функции (точка входа) */
 {
-	printf ("Привет мир!!!\n"); /* печать в стандартный вывод */
+	printf (_("Hello world!!!\n")); /* печать в стандартный вывод */
   return 0; /* возвращаем код ошибки если ошибки нет то 0 */
 }
--- 001/main.c
+++ 001/main.c
@@ -1,11 +1,39 @@
+/*!
+Коментарий для системы автоматического документирования
+\authors   Олег Лесков <oleg@oSvoj.ru>
+\version   0.0.1a
+\date      09.2022
+\bug       Известные ошибки
+\warning   Данный код написан в учебных целях
+\copyright MIT
+\example   приеры использования
+\todo      Что нужно сделать
+\brief     Краткое описание
+\details   Подробное описание
+*/
 /*
 main.c
 author: oleg <oleg@osvoj.ru>
 license: MIT
 */
+/// коментарий для системы автоматического документирования
+//однострочный коментарий
+
 #include "hello.h"
+#include <libintl.h>
+#define _(String) gettext (String)
+#define PACKAGE "hello.osvoj.ru"
+#define LOCALEDIR "locale"
+#include <stdlib.h>
+
 
 int main (void) /* объявление основной функции (точка входа) */
 {
-  return hello(); /* возвращаем код ошибки если ошибки нет то 0 */
+  setlocale (LC_ALL, getenv("LC_CTYPE"));
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  textdomain (PACKAGE);
+
+  int a;
+  a = hello();
+  return a; /* возвращаем код ошибки если ошибки нет то 0 */
 }
